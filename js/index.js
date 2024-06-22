451969117f5d87a1ae0727bd47afa876637a1c69
Numonovdev <ebxuzsher@gmail.com>
2024-06-22 08:32:55 +0300

    ..

---
+++ js/index.js
@@ -0,0 +1,181 @@
+// Masalalar
+
+// 1-masala
+
+// let colors = ['color-1', 'color-2', 'color-3'];
+// console.log(colors[1])
+
+// 2-masala
+
+// let numbers = ['num-1', 'num-2', 'num-3'];
+// console.log(numbers[1])
+
+// 3-masala
+
+
+
+
+
+// 4-masala
+
+// let element = ['one', 'two', 'three', 'four'];
+//  element[1] = 'fine'
+//  console.log(element)
+
+// 5-masala
+
+// let element = ['one', 'two', 'three', 'four'];
+//  element.push = 'fine'
+//  console.log(element)
+
+// 6-masala
+
+// let element = ['one', 'two', 'three', 'four'];
+//  element.pop()
+//  console.log(element)
+
+
+// 7-masala
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+//  console.log(element.length)
+
+// 8-masala
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+// element.unshift('zero')
+//  console.log(element)
+
+// 9-masala
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+// element.shift()
+//  console.log(element)
+
+
+// 10-masala
+
+
+
+
+
+// 11-masala
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+//  console.log(element.includes('four'));
+
+
+// 12-Masala
+
+
+
+
+// 13-masala
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+//  console.log(element.indexOf('four'));
+
+// 14-masala
+
+
+
+
+
+
+
+// 15-masala
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+// element.pop();
+// element.pop();
+// console.log(element);
+
+
+// 16-masala
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+// element.push('eleven');
+// element.unshift('zero')
+// console.log(element);
+
+// 17-masala
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 18-masala
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+//  console.log(element.splice(element.length));
+
+
+// 19-masala
+
+
+
+// 20-masala
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+// for(let i = 0; i <= element.length; i++){
+//      console.log(element[i]);
+//      console.log(element[i]);
+// }
+
+
+// 21-masala
+
+
+
+
+
+
+
+// 22-masala
+
+
+
+
+// 23-masala
+
+
+
+
+
+
+
+// masala-24
+
+// let element = ['one', 'two', 'three', 'four', 'fine', 'six', 'seven', 'eight', 'nine', 'ten'];
+// for(let i = element.length; i >= 0; i--){
+//      console.log(element[i]);
+// }
+
+
+// 25-masala
+
+
+// let element = ['1', '2', '3', '4', '5', '6', 'seven', 'eight', 'nine', 'ten'];
+// for(let i = 1; i <= element.length; i += 2){
+      
+//      console.log(element[i]);
+// }
+
+// 26-masala
+
+
+// let element = ['1', '2', '3', '4', '5', '6', 'seven', 'eight', 'nine', 'ten'];
+// for(let i = 0; i <= element.length; i++){
+//       let a = i * i;
+//      console.log(a);
+// }
+
+
+// 27-masala
+
